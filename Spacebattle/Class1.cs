aee0f2a7d9eeee01dd9f482e3bc2c77fd8ff0622
Musiyak Egor <musiyak26@mail.ru>
2023-06-30 10:51:35 +0000

    ship

--- Spacebattle/Class1.cs
+++ Spacebattle/Class1.cs
@@ -1,13 +1,38 @@
 ﻿namespace UtilityLibraries;
 
-public static class StringLibrary
+public class Ship 
 {
-    public static bool StartsWithUpper(this string? str)
+    public double x { get; set; }
+    public double y { get; set; }
+    public double speed_x { get; set; }
+    public double speed_y { get; set; }
+    public bool move_possible { get; set; }
+}
+public class Spacebattle
+{
+    private Ship ship = new Ship();
+    public Ship Move(Ship ship)
     {
-        if (string.IsNullOrWhiteSpace(str))
-            return false;
+        if (ship.x == double.NaN || ship.y == double.NaN)
+        {
+            throw new System.ArgumentException();
+        }
 
-        char ch = str[0];
-        return char.IsUpper(ch);
+        if (ship.speed_x == double.NaN || ship.speed_y == double.NaN)
+        {
+            throw new System.ArgumentException();
+        }
+
+        if (ship.move_possible == false)
+        {
+            throw new System.ArgumentException();
+        }
+
+        ship.x -= ship.speed_x;
+        ship.y -= ship.speed_y;
+
+        return ship;
+
+        throw new NotImplementedException();
     }
 }
