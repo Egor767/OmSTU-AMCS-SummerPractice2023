b693444609c559ca2f0ef6a268b8c008db515568
Egor Musiyak <64477831+Egor767@users.noreply.github.com>
2023-10-20 11:36:01 +0100

    1

--- SpaceBattle/Commands/TurnCommand.cs
+++ SpaceBattle/Commands/TurnCommand.cs
@@ -1,18 +1,13 @@
 ﻿namespace SpaceBattle;
 public class Turn : ICommand
 {
-    private Iturnable turnable;
-    public Turn(Iturnable turnable)
+    private ITurnable turnable;
+    public Turn(ITurnable turnable)
     {
         this.turnable = turnable;
     }
     public void Execute()
     {
-        if (turnable.angle.IsNan())
-            throw new Exception("Wrong angle!");
-        if (turnable.angle_velocity.IsNan())
-            throw new Exception("Wrong angle velocity!");
-
         turnable.angle += turnable.angle_velocity;
     }
 }
--- SpaceBattle/Interfaces/Iturnable.cs
+++
@@ -1,7 +0,0 @@
-namespace SpaceBattle;
-
-public interface Iturnable
-{
-    public Vector angle {get; set;}
-    public Vector angle_velocity {get;}
-}
